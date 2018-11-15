5116fe915cb6c36c0b3388778f6da2b4c44abaf1
Renee Fowler <reneemfowler@Renees-MBP.hitronhub.home>
2018-11-15 13:08:28 -0800

    created schedule and schedule-item models and corresponding admin

--- bb_schedule/admin.py
+++ bb_schedule/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Schedule, ScheduleItem
 
-# Register your models here.
+
+class ScheduleAdmin(admin.ModelAdmin):
+    model = Schedule
+    prepopulated_fields = {'slug': ('title',)}
+    ordering = ('title',)
+    search_fields = ('title',)
+
+class ScheduleItemAdmin(admin.ModelAdmin):
+    model = ScheduleItem
+    list_display = ('time', 'title', 'notes', 'schedule')
+    ordering = ('schedule',)
+    search_fields = ('schedule', 'time', 'title',)
+
+    
+admin.site.register(Schedule, ScheduleAdmin)
+admin.site.register(ScheduleItem, ScheduleItemAdmin)
--- bb_schedule/models.py
+++ bb_schedule/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Schedule(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+    description = models.TextField(blank=True)
+    
+    def __str__(self):
+        return self.title
+    
+
+class ScheduleItem(models.Model):
+    schedule = models.ForeignKey(Schedule, on_delete=models.CASCADE,)
+    time = models.TimeField()
+    title = models.CharField(max_length=255)
+    notes = models.TextField(blank=True)
+    
+    def __str__(self):
+        return self.title
