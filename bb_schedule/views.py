4dceab96cb061178b3cd6c77721368416f3c0c55
Renee Fowler <reneemfowler@Renees-MBP.hitronhub.home>
2018-11-16 12:06:56 -0800

    created base template and index template

--- bb_schedule/views.py
+++ bb_schedule/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Schedule
 
-# Create your views here.
+
 def index(request):
-    return HttpResponse("<h1>Welcome to Bubbly Bambino</h1>")
+    schedules = Schedule.objects.all()
+    return render(request, 'bb_schedule/index.html', {
+        'schedules': schedules,
+    })
